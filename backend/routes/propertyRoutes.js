18b8832a015f5ad8e3cb66417caa06a1c1915c27
Aditi Bansal <aditiib7@gmail.com>
2026-07-14 19:43:03 +0530

    feat: add property update and getby id routes

--- backend/routes/propertyRoutes.js
+++ backend/routes/propertyRoutes.js
@@ -4,9 +4,15 @@ const router = express.Router();
 const {
   addProperty,
   getProperties,
+  getPropertyById,
+  updateProperty, 
 } = require("../controllers/propertyController");
+
 const { protect } = require("../middleware/authMiddleware");
 
 router.post("/", protect, addProperty);
 router.get("/", getProperties);
+router.get("/:id", getPropertyById);
+router.put("/:id", protect, updateProperty);
+
 module.exports = router;
